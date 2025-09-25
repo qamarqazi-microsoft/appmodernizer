d8688b07e9c10930d95b7fe4c1e649a40ffc6096
qamarqazi-microsoft <qamarqazi@outlook.com>
2025-09-25 10:48:53 +0530

    Update summarizer.py

--- skills/BusinessLogicSummarizer/summarizer.py
+++ skills/BusinessLogicSummarizer/summarizer.py
@@ -27,7 +27,7 @@ class BusinessLogicSummarizer:
             client = AzureOpenAI(
                 api_key=os.getenv("AZURE_OPENAI_API_KEY"),
                 api_version="2024-12-01-preview",
-                azure_endpoint="https://azure-ai-foundry-vijay.openai.azure.com/"
+                azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
             )
             response =  client.chat.completions.create(
                 model="gpt-4.1",
